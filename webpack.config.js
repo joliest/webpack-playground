87f05a283c02449343ddac354cdfa2c70bf24974
Joliver Estampador <joliverestampador@gmail.com>
2022-11-11 07:43:21 +0800

    adding output property

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 const config = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    // save the bundle.js in our project directory and build folder
+    path: path.resolve(__dirname, 'build'), // should be an absolute path. NOT relative path. use 'path' from node
+    filename: 'bundle.js', // you can name this anything. This is just convention
+  },
 };
 
 module.exports = config;
