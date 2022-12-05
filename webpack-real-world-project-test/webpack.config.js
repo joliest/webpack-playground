867abf79f993df14e59302919c9bc2c01bfbd792
Joliver Estampador <joliverestampador@gmail.com>
2022-12-05 08:00:46 +0800

    applying babel and styles
    https://docs.google.com/document/d/16WdDmZeGQ4x7r2K4Da5gCEK4yq5Nsa9VhyUGHU53Wa8/edit#heading=h.alrfg7girj7i

--- webpack-real-world-project-test/webpack.config.js
+++ webpack-real-world-project-test/webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        /**
+         * do not try to apply babel in any files inside node_modules
+         * It will still work fine without this, but we need to 
+         * prevent wasting of resources
+         */
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
   }
 };
