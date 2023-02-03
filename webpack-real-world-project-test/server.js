3dcf8f16621dfb02950fc56aff716085e56ab900
Joliver Estampador <joliverestampador@gmail.com>
2023-02-03 16:55:01 +0800

    getting our server work on production

--- webpack-real-world-project-test/server.js
+++ webpack-real-world-project-test/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js')
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// NODE_ENV should be set properly
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js')
+  // use webpack when it's not production
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  // when running in production.
+  // go the user and let the user to have access in the dist folder
+  app.use(express.static('dist'))
+  app.get('*', (req, res) => {
+    // handling edge case, redirects when user enters invalid route
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  })
+}
 
-app.listen(3000, () => {
-  console.log('RUnning at port 3000')
+app.listen(process.env.PORT || 3050, () => {
+  console.log('Listening . . .')
 })
